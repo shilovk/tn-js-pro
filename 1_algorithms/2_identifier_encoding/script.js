fb6f844d05d6ce7a05c537b32f018d0c84809fc5
ShilovK <shilovk@gmail.com>
2020-05-13 03:34:47 +0300

    2 id encoding by format AAAAAAAABBCDDDDDD

---
+++ 1_algorithms/2_identifier_encoding/script.js
@@ -0,0 +1,16 @@
+const timestamp = Math.floor(Date.now() / 1000);
+const a = timestamp.toString(16);
+
+const cluster = 10;
+const b = cluster.toString(16);
+
+const type = 5;
+const c = type.toString(16);
+
+const user = 1234567;
+const d = user.toString(16);
+
+const id = a + b + c + d;
+
+console.log(timestamp, cluster, type, user);
+console.log(`id encoding by format AAAAAAAABBCDDDDDD : ${id}`);
